57e4c39c84e0d5801d9c8de09668ab350ef2a8e1
AlexNocua <nocua68@gmail.com~>
2024-08-20 22:27:54 -0500

    Primeiras configuracoes do entorno de trabalho para olhar a mesma sentaxis

---
+++ sintaxis_java/src/App.java
@@ -0,0 +1,16 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int num = 1;
+        int num2 = 2;
+
+        System.out.print(suma(num, num2));
+
+    }
+
+    public static int suma(int num1, int num2) {
+        return num1 + num2;
+
+    }
+}
